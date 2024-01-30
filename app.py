95fd72fa5e5c34fbede0787ec048032e356b5d63
jennAllen <51343347+jennAllen@users.noreply.github.com>
2024-01-30 12:12:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
-st.title(" Hello World - Jenn Test")
+st.title(" Hello World - Jenn Test - Update Jan 30 - User B Is View")
 st.title(" THIS IS AN UPDATE - REPUBLISH ME")
 st.title("Top 5%" " INCOME SHARE")
 st.markdown("Share of income received by the richest 5%" " of the population.")
