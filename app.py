916786c34a9ed9a2244ce09164650060f292052d
Jenn Allen <51343347+jennAllen@users.noreply.github.com>
2026-04-17 16:43:33 -0700

    Change title from 'Hello World' to 'BONJOUR TOUT LA MONDE'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
-st.title(" Hello World - Jenn Test")
+st.title(" BONJOUR TOUT LA MONDE")
 st.title(" Another updated Thank you autopublish")
 st.title("Top 5%" " INCOME SHARE")
 st.markdown("Share of income received by the richest 5%" " of the population.")
